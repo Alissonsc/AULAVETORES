b34c82eebad05994b03b9c1da86766e4f5a243f6
alissonsc <alisson.csilva16@senacsp.edu.br>
2024-08-30 21:38:43 -0300

    feat: Exercício 1 concluído

---
+++ ex1.js
@@ -0,0 +1,14 @@
+//let num [5, 6, 7, 8, 9, 10];
+
+let num = [];
+//leitura dos numeros nos espaços
+num[0] = prompt("Informe o 1° Número");
+num[1] = prompt("Informe o 2° Número");
+num[2] = prompt("Informe o 3° Número");
+num[3] = prompt("Informe o 4° Número");
+num[4] = prompt("Informe o 5° Número");
+
+let mensagem =
+    `${num[0]}, ${num[1]}, ${num[2]}, ${num[3]} e ${num[4]} `;
+
+alert(mensagem);
---
+++ ex1v2.js
@@ -0,0 +1,7 @@
+//let num= [5, 6, 7, 8, 9, 10];
+
+let num =[];
+//leitura dos numeros nos espaços
+for (let i = 0; i < 5; i++) {
+
+}
