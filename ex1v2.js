743a369603b89ebc72306f3a867ec142f876ee9e
alissonsc <alisson.csilva16@senacsp.edu.br>
2024-09-02 21:22:01 -0300

    feat: exercício 1 versao 2 corrigido.

--- ex1v2.js
+++ ex1v2.js
@@ -1,7 +1,24 @@
 //let num= [5, 6, 7, 8, 9, 10];
 
-let num =[];
-//leitura dos numeros nos espaços
-for (let i = 0; i < 5; i++) {
-
+let num = [];
+let mensagem = "";
+let numerostr = "";
+// leitura dos numeros nos espaços
+for (let i = 0; i < 5; i += 1) {
+    numerostr = prompt("Informe um número:");
+    num.push(numerostr);
 }
+
+for (let i = 0; i < num.length; i++) {
+
+    // O total de Elementos - 1 equivale a ultima posição do vetor.
+    // Ex: 5 valores no vetor, vai de 0 a 4 posições [0,1,2,3,4]. 
+    // O objetivo é inserir o ponto final no ultimo valor no texto.
+    let ultimaPosicao = (num.length - 1); 
+    
+    if(i == ultimaPosicao)
+        mensagem += num[i] + ".";
+    else
+        mensagem += num[i] + ", ";    
+}
+alert(mensagem);
