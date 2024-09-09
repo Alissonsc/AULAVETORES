f1c39a30c6c7625b1ede5e142a14ff9e6fc91728
alissonsc <alisson.csilva16@senacsp.edu.br>
2024-09-09 20:00:36 -0300

    feat: exercício 3 concluído e corrigido

---
+++ ex2.js
@@ -0,0 +1,10 @@
+/*Crie um algoritmo que leia 4 notas, mostre as notas e a média na tela.*/
+let nota = [];
+
+nota[0] = prompt("Informe a 1° Nota");
+nota[1] = prompt("Informe a 2° Nota");
+nota[2] = prompt("Informe a 3° Nota");
+nota[3] = prompt("Informe a 4° Nota");
+nota[4] = prompt("Informe a 5° Nota");
+
+let media = 
