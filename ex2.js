3683675683b2668c3c4053b77f5602e8c21a28bf
alissonsc <alisson.csilva16@senacsp.edu.br>
2024-09-09 21:53:15 -0300

    feat: exercício 2 corrigido

--- ex2.js
+++ ex2.js
@@ -1,10 +1,33 @@
 /*Crie um algoritmo que leia 4 notas, mostre as notas e a média na tela.*/
-let nota = [];
 
-nota[0] = prompt("Informe a 1° Nota");
-nota[1] = prompt("Informe a 2° Nota");
-nota[2] = prompt("Informe a 3° Nota");
-nota[3] = prompt("Informe a 4° Nota");
-nota[4] = prompt("Informe a 5° Nota");
+let notas = [];
+let soma = 0;
+let media = 0;
 
-let media = 
+for (let i = 0; i < 4; i++) {
+    let contagem = (i + 1);
+    let nota = parseFloat(
+        prompt(`Informe a ${contagem}ª Nota do Aluno: `)
+    );
+    notas.push(nota);
+    // Obs: Você pode também fazer: 
+    // notas[i] = nota; 
+    // isso funciona também
+}
+
+//Isso é a quantidade de notas inseridas no vetor.
+const quantNotas = notas.length;
+let notasMsg = "";
+for (let j = 0; j < quantNotas; j++) {
+    soma += notas[j];
+    //Se o valor de J na repetição é a ultima posição do vetor
+    if (j == quantNotas - 1)
+        notasMsg += notas[j] + ".";
+    else
+        notasMsg += notas[j] + ", ";
+}
+
+media = soma / quantNotas;
+
+alert(`A notas informadas foram: ${notasMsg} 
+       Média dessas notas foi ${media.toFixed(2)} `);
